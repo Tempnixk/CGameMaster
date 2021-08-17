c520a1423ea0caa3922611f62ad0cbbd569aea44
unknown <uudquark@soongsil.ac.kr>
2021-08-17 16:07:49 +0900

    8/17
    0. Environment Setting
    1. Calculator

---
+++ 1. Calculator - 4/1. Calculator - 4/1. Calculator - 4.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	int firstValue, secondValue;
+	int addResult, subResult, multiResult;
+	float devideResult;
+
+
+	printf("input 2 data\n");
+	scanf("%d %d", &firstValue, &secondValue);
+	addResult = firstValue + secondValue;
+	subResult = firstValue - secondValue;
+	multiResult = firstValue * secondValue;
+	devideResult = (float)firstValue / secondValue;
+
+	printf("%d + %d = %d\n", firstValue, secondValue, addResult);
+	printf("%d - %d = %d\n", firstValue, secondValue, subResult);
+	printf("%d * %d = %d\n", firstValue, secondValue, multiResult);
+	printf("%d / %d = %f\n", firstValue, secondValue, devideResult);
+
+
+	return 0;
+}
---
+++ 1. Calculator/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	int firstInput, secondInput;
+	int addResult, subResult, multiResult;
+	float devideResult;
+	int max;	
+
+
+	printf("input 2 integer data\n");
+	scanf("%d %d", &firstInput, &secondInput);
+	addResult = firstInput + secondInput;
+	subResult = firstInput - secondInput;
+	multiResult = firstInput * secondInput;
+	devideResult = (float) firstInput / secondInput;
+
+
+	if (firstInput > secondInput) {
+		max = firstInput;
+	}
+	else {
+		max = secondInput;
+	}
+	printf("%d+%d=%d\n", firstInput,secondInput,addResult);
+	printf("%d-%d=%d\n", firstInput, secondInput, subResult);
+	printf("%d*%d=%d\n", firstInput, secondInput, multiResult);
+	printf("%d*%d=%f\n", firstInput, secondInput, devideResult);
+	printf("max: %d\n", max);
+	return 0;
+}
